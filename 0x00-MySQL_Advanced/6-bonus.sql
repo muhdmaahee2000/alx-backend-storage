525f50f27a147f19c4304e9ae820351de7d0cf15
muhdmaahee2000 <youngzmahi99@gmail.com>
2023-07-15 00:18:07 +0100

    creates tables, ranks country origins of bands,lists all bands with Glam rock as their main style,creates a trigger that decreases.

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,24 +1,16 @@
-DELIMITER //
-
+-- Script that creates a stored procedure AddBonus
+-- that adds a new correction for a student.
+DELIMITER $$ ;
 CREATE PROCEDURE AddBonus(
-    IN p_user_id INT,
-    IN p_project_name VARCHAR(255),
-    IN p_score DECIMAL(10, 2)
+	IN user_id INTEGER,
+	IN project_name VARCHAR(255),
+	IN score INTEGER
 )
 BEGIN
-    DECLARE project_id INT;
-    
-    -- Check if the project already exists in the projects table
-    SELECT id INTO project_id FROM projects WHERE name = p_project_name;
-    
-    -- If the project doesn't exist, create it and get its ID
-    IF project_id IS NULL THEN
-        INSERT INTO projects (name) VALUES (p_project_name);
-        SET project_id = LAST_INSERT_ID();
-    END IF;
-    
-    -- Insert the new correction for the student
-    INSERT INTO corrections (user_id, project_id, score) VALUES (p_user_id, project_id, p_score);
-END//
-
+	IF NOT EXISTS(SELECT name FROM projects WHERE name=project_name) THEN
+		INSERT INTO projects (name) VALUES (project_name);
+	END IF;
+	INSERT INTO corrections (user_id, project_id, score)
+	VALUES (user_id, (SELECT id from projects WHERE name=project_name), score);
+END;$$
 DELIMITER ;
