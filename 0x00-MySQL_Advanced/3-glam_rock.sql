ab5df84ed096df63cf7dd069596bfa89b14fa1c9
muhdmaahee2000 <youngzmahi99@gmail.com>
2023-07-15 00:31:54 +0100

    creates tables, ranks country origins of bands,lists all bands with Glam rock as their main style,creates a trigger that decreases.

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,18 +1,5 @@
--- Step 1: Create a temporary table with the calculated lifespan for each band
-CREATE TEMPORARY TABLE temp_band_lifespan AS
-SELECT
-    band_name,
-    (CASE
-        WHEN split IS NOT NULL THEN YEAR(split) - YEAR(formed)
-        ELSE 2022 - YEAR(formed)
-    END) AS lifespan
-FROM metal_bands;
-
--- Step 2: List all bands with Glam rock as their main style, ranked by their longevity
-SELECT
-    band_name,
-    lifespan
-FROM temp_band_lifespan
-WHERE band_name IN (SELECT band_name FROM metal_bands WHERE main_style = 'Glam rock')
-ORDER BY lifespan DESC;
-
+-- This SQL script select band_name, and lifespan column which is difference
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
